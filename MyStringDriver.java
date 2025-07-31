cdc8114fa485786234e60d7a53542928189ddf7a
Amey Patil <97515279+Amey11github@users.noreply.github.com>
2025-07-31 22:15:23 +0530

    Update: final MyStringDriver.java

--- MyStringDriver.java
+++ MyStringDriver.java
@@ -1,3 +1,4 @@
+package MyStr;
 
 import java.util.*;
 
@@ -13,23 +14,24 @@ final class MyString{
 	
 char [] arr;
 	
-	public MyString() {
+	public MyString() //1
+	{
 		arr = new char[0];
 		
 	}
 	
-	public MyString(String str)
+	public MyString(String str)//2
 	{
 		arr=new char[str.length()];
 		for(int i=0;i<str.length();i++)
 			arr[i]=str.charAt(i);
 	}
 	
-	public MyString(char [] arr)
+	public MyString(char [] arr)//3
 	{
 		this.arr=arr.clone();
 	}
-	public MyString(byte [] arr)
+	public MyString(byte [] arr)//4
 	{
 		this.arr=new char[arr.length];
 		
@@ -39,7 +41,7 @@ char [] arr;
 		}
 	}
 	
-	public MyString(char [] arr,int start,int count)
+	public MyString(char [] arr,int start,int count)//5
 	{
 		if((start+count)>arr.length)
 		    throw new MyStringIndexOutOfBoundException("INVALID ["+start+" ,"+start+"+"+count+"] out of bounds for length "+arr.length);
@@ -50,30 +52,51 @@ char [] arr;
 		    	this.arr[j]=arr[i];
 		
 	}
+	public MyString(byte[] arr, int start, int count) //6
+	{
+	    if (start < 0 || count < 0 || start + count > arr.length) {
+	        throw new MyStringIndexOutOfBoundException("INVALID [" + start + ", " + start + "+" + count + "] out of bounds for length " + arr.length);
+	    }
+	    this.arr = new char[count];
+	    for (int i = 0, j = start; i < count; i++, j++) {
+	        this.arr[i] = (char) arr[j];
+	    }
+	}
+	
+	public MyString(int[] arr, int start, int count) //7
+	{
+	    if (start < 0 || count < 0 || start + count > arr.length) {
+	        throw new MyStringIndexOutOfBoundException("INVALID [" + start + ", " + start + "+" + count + "] out of bounds for length " + arr.length);
+	    }
+	    this.arr = new char[count];
+	    for (int i = 0, j = start; i < count; i++, j++) {
+	        this.arr[i] = (char) arr[j];
+	    }
+	}
 
-	public MyString(StringBuffer sb)
+	public MyString(StringBuffer sb)//8
 	{
 		this(sb.toString());
 	}
 	
-	public MyString(StringBuilder sb)
+	public MyString(StringBuilder sb)//9
 	{
 		this(sb.toString());
 	}
-	
-	public String toString()
+	//---------------------------------------------------------------
+	public String toString()//1
 	{
 		String str="";
 		for(char ele:arr) str+=ele;
 		return str;
 	}
 	
-	public int length()
+	public int length()//2
 	{
 		return arr.length;
 	}
 	
-	public int indexOf(int ch)
+	public int indexOf(int ch)//3
 	{
 		if(arr.length>0)
 		{
@@ -82,12 +105,12 @@ char [] arr;
 		return -1;
 	}
 	
-	public boolean isEmpty()
+	public boolean isEmpty()//4
 	{
 		return arr.length>0 ? false : true;
 	}
 	
-	public MyString toUpperCase()
+	public MyString toUpperCase()//5
 	{
 		char[] newArr=new char[arr.length];
 		for(int i=0;i<arr.length;i++)
@@ -98,7 +121,7 @@ char [] arr;
 		return new MyString(newArr);
 	}
 	
-	public MyString toLowerCase()
+	public MyString toLowerCase()//6
 	{
 		char[] newArr=new char[arr.length];
 		for(int i=0;i<arr.length;i++)
@@ -109,7 +132,7 @@ char [] arr;
 		return new MyString(newArr);
 	}
 	
-	public boolean startsWith(MyString prefix,int start)
+	public boolean startsWith(MyString prefix,int start)//7
 	{
 		if(prefix.length()>arr.length || prefix.length()>(arr.length-start))
 			return false;
@@ -120,12 +143,12 @@ char [] arr;
 		return true;
 	}
 	
-	public boolean startsWith(MyString prefix)
+	public boolean startsWith(MyString prefix)//8
 	{
 		return startsWith(prefix,0);
 	}
 	
-	public boolean contains(MyString str)
+	public boolean contains(MyString str)//9
 	{
 		for(int i=0;i<arr.length;i++)
 		
@@ -135,7 +158,7 @@ char [] arr;
 		return false;	
 	}
 	
-	public MyString substring(int start,int end)
+	public MyString substring(int start,int end)//10
 	{
 		if(start<end || start<0 || end<0 || end>arr.length)
 			throw new MyStringIndexOutOfBoundException("Range ["+start+","+end+"] out of bounds for length "+arr.length);
@@ -147,7 +170,7 @@ char [] arr;
 		return new MyString(newArr);
 	}
 
-	public boolean endsWith(MyString suffix)
+	public boolean endsWith(MyString suffix)//11
 	{
 		if(arr.length<suffix.length())return false;
 		
@@ -157,7 +180,7 @@ char [] arr;
 		return true;
 	}
 	
-	public boolean equals(Object obj)
+	public boolean equals(Object obj)//12
 	{
 		if(!(obj instanceof MyString))return false;
 		MyString str=(MyString)obj;
@@ -170,18 +193,18 @@ char [] arr;
 		return true;
 	}
 	
-	public boolean equalsIgnoreCase(MyString str)
+	public boolean equalsIgnoreCase(MyString str)//13
 	{
 		return this.toLowerCase().equals(str.toLowerCase());
 	}
 	
-	public boolean contentEquals(StringBuffer sb)
+	public boolean contentEquals(StringBuffer sb)//14
 	{
 		MyString obj=new MyString(sb);
 		return this.equals(obj);
 	}
 	
-	public char charAt(int indx)
+	public char charAt(int indx)//15
 	{
 			if(indx<0 || indx >=arr.length)
 			{
@@ -190,7 +213,7 @@ char [] arr;
 		return arr[indx];
 	}
 	
-	public int codePointAt(int indx)
+	public int codePointAt(int indx)//16
 	{
 		if(indx<0 || indx >=arr.length)
 		{
@@ -199,12 +222,12 @@ char [] arr;
 		return arr[indx];
 	}
 	
-	public int codePointBefore(int indx)
+	public int codePointBefore(int indx)//17
 	{
 		return codePointAt(indx-1);
 	}
 	
-	public int codePointCount(int start,int end)
+	public int codePointCount(int start,int end)//18
 	{
 		if(start>end || start<0 || end<0 || start>=arr.length)
 		{
@@ -214,7 +237,7 @@ char [] arr;
 		return end-start;
 	}
 	
-	public MyString concat(MyString merge)
+	public MyString concat(MyString merge)//19
 	{
 		char[] newArr=new char[arr.length+merge.length()];
 		int indx=0;
@@ -228,7 +251,7 @@ char [] arr;
 		
 	}
 	
-	public MyString replace(char oldChar,char newChar)
+	public MyString replace(char oldChar,char newChar)//20
 	{
 		char [] newArr=arr.clone();
 		for(int i=0;i<arr.length;i++)
@@ -239,6 +262,128 @@ char [] arr;
 		}
 		
 		return new MyString(newArr);
+	}
+	
+	public int indexOf(int assci,int start)//21
+	{
+		for(int i=0;i<arr.length;i++)
+		{
+			if(assci==arr[i])return i;
+		}
+		return -1;
+	}
+	
+	public MyString[] split(MyString regex)//22 
+	{
+	    int count = 0;
+	    for (int i = 0; i < arr.length; i++) {
+	        if (arr[i] == regex.charAt(0)) count++;
+	    }
+
+	    MyString[] newArr = new MyString[count + 1];
+	    int indx = 0;
+
+	    // Commented example: "JAVA IS EASY JAVA IS PROG LANG JAVA IS HLL"
+	    MyString str = new MyString(""); 
+
+	    for (char ele : arr) {
+	        if (ele != regex.charAt(0)) {
+	            str = str.concat(new MyString(ele + ""));
+	        } else {
+	            newArr[indx++] = str;
+	            str = new MyString(""); 
+	        }
+	    }
+
+	    newArr[indx] = str;
+	    return newArr;
+	}
+	
+	public MyString trim() //23
+	{
+        int left = 0, right = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == ' ') left++;
+            else break;
+        }
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] == ' ') right++;
+            else break;
+        }
+
+        return substring(left, arr.length - right);
+    }
+
+	
+	public MyString replaceAll(MyString searchStr, MyString repStr) //24
+	{
+	    MyString[] newArr = new MyString(arr).split(new MyString(" "));
+	    System.out.println(Arrays.toString(newArr));
+	    MyString op = new MyString("");
+	    int indx = 0;
+
+	    for (MyString ele : newArr) {
+	        if (ele.equals(searchStr)) {
+	            newArr[indx] = repStr;
+	        }
+	        op = op.concat(new MyString(newArr[indx++] + " "));
+	    }
+
+	    return op.trim();
+	}
+	
+	public MyString substring(int offset) //25
+	{
+		if (offset==0) return new MyString(arr);
+		
+		return substring(offset,arr.length);
+	}
+	
+	public MyString replaceFirst(MyString searchStr, MyString repStr) //26
+	{
+		MyString[] newArr=new MyString(arr).split(new MyString(" "));
+
+		MyString op =new MyString("");
+		
+		int indx=0;
+		boolean flag=true;
+		for (int i = 0; i < newArr.length; i++) {
+			if(newArr[i].equals(searchStr) && flag==true) {
+				newArr[indx]=repStr;
+				flag=false;
+			}
+			op=op.concat(new MyString(newArr[indx++]+" "));
+
+		}
+		
+		return op.trim();
+	}
+	
+	public int lastIndexOf(int ch, int startIndx)//27
+	{
+		char ele=(char)ch;
+		if(startIndx>=arr.length)
+			startIndx=arr.length-1;
+		
+		for (int i = startIndx; i>=0; i--) {
+			if (arr[i]==ele) {
+				return i;
+			}
+		}
+		
+		return -1;
+	}
+	
+	public int lastIndexOf(int ch)//28
+	{
+		return  lastIndexOf(ch,arr.length);
+	}
+	
+	public char[] toCharArray() //29
+	{
+	    return arr.clone(); 
 	}
 	
 }
@@ -276,23 +421,25 @@ public class MyStringDriver {
 		MyString mstr7=new MyString(barr);
 		System.out.println(mstr7);
 		
-		System.out.println(str.length());
+//		System.out.println(str.length());
 		MyString mstr8=new MyString("hello");
 		MyString mstr10=new MyString("HELLO");
 		MyString mstr9=new MyString("");
-		System.out.println(mstr8.length());
-		System.out.println(mstr8.isEmpty());
+		System.out.println(mstr8.length());//1
+		System.out.println(mstr8.isEmpty());//2
 		System.out.println(mstr9.isEmpty());
-		System.out.println(mstr8.charAt(3));
+		System.out.println(mstr8.charAt(3));//3
 		System.out.println(mstr8.charAt(2));
-		System.out.println(mstr8.codePointAt(4));
-		System.out.println(mstr8.codePointBefore(4));
-		System.out.println(mstr8.codePointCount(1,3));
-		System.out.println(mstr8.toUpperCase());
-		System.out.println(mstr10.toLowerCase());
-		System.out.println(mstr8.concat(mstr10));
-		System.out.println(mstr10.startsWith(new MyString("H"), 0));
-
+		System.out.println(mstr8.codePointAt(4));//4
+		System.out.println(mstr8.codePointBefore(4));//5
+		System.out.println(mstr8.codePointCount(1,3));//6
+		System.out.println(mstr8.toUpperCase());//7
+		System.out.println(mstr10.toLowerCase());//8
+		System.out.println(mstr8.concat(mstr10));//9
+		System.out.println(mstr10.startsWith(new MyString("H"), 0));//10
+		
+		System.out.println(str.codePointAt(4));
+		
 
 		
 	}
